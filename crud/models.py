4fe385cc26795a1d36bca634577ae1f6bca7a8b2
narmuhamedov <kingradomir@gmail.com>
2025-03-14 18:39:44 +0600

    '4lesson'

---
+++ crud/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from films.models import Films
+
+class TodoList(models.Model):
+    STATUS = (
+        ('✅', '✅'),
+        ('❌', '❌')
+    )
+    title = models.CharField(max_length=100)
+    choice_films = models.ForeignKey(Films, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS, default='❌')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
--- main_app/settings.py
+++ main_app/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'films',
     'hastags',
+    'crud',
 ]
 
 MIDDLEWARE = [
